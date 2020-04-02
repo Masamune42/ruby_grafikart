f54d6e3d4da36527abb78d018760357b0edb0819
alex <a.caignard@hotmail.fr>
2020-04-02 23:23:56 +0200

    variables

--- exo1.rb
+++ exo1.rb
@@ -1,1 +1,8 @@
-puts "Salut la planète"
+# chomp -> retire le saut de ligne
+# nom = gets.chomp
+# puts "Salut #{nom} comment allez vous?"
+
+# Faire le to_i après car chomp converti en string
+puts "Entrez un chiffre"
+chiffre = gets.chomp.to_i
+puts chiffre.even?
